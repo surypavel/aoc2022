bb8027a149d7ec0d1d2af9918306897b84b9d1cb
Pavel Surý <pavel.sury@rossum.ai>
2022-12-20 19:09:15 +0100

    Finish day 19

--- 19a.hs
+++ 19a.hs
@@ -19,47 +19,50 @@ unsafeParse = (map unsafeParseLine) . lines
 unit :: Int -> [Int]
 unit n = (replicate n 0) ++ 1:(repeat 0)
 
-isAffordable :: State -> Bool
-isAffordable (_, _, cash, _) = all (>=0) cash
+isMeaningfulUpgrade :: [Resources] -> Resources -> Int -> Bool
+isMeaningfulUpgrade costs _ 3 = True
+isMeaningfulUpgrade costs x i = not $ all ((<=factories) . (!! i)) costs
+    where factories = x !! i
+
+-- heuristics
+-- do not attempt to upgrade something i refused to upgrade earlier
+-- buy geodes when you can
+-- do not upgrade if you are generating enough to cover any cost
 
 nextStates :: [Resources] -> State -> [State]
-nextStates b (t, fac, res, sk) = if length purchaseStates == 4 then [last purchaseStates] else waitState:purchaseStates
-    where waitState = (t+1, fac, zipWith (+) res fac, map snd $ filter ((==True) . fst) $ zip (map isAffordable options) [0..])
-          skipped = \(_,i) -> i `notElem` sk
-          options = map (\(cost,i) -> (t+1, zipWith (+) fac (unit i), zipWith (-) res (cost), sk)) $ filter skipped $ zip b [0,1..]
-          purchaseStates = map (\(x,y,z,_) -> (x,y,zipWith (+) z fac, [])) $ filter isAffordable options
+nextStates b (t, fac, res, sk) = if canBuyGeode then [buyGeode] else waitState:purchaseStates
+    where waitState = (t+1, fac, zipWith (+) res fac, unusedOptions)
+          resources = zip b [0,1..]
+          unusedOptions = map snd $ filter ((==True) . fst) $ zip (map affordable $ map spend resources) [0..]
 
-hasObsidian :: State -> Bool
-hasObsidian (_, (_:_:x:_), _, _) = x > 0
+          notSkipped = \(_,i) -> i `notElem` sk
+          isUpgrade = \(_,i) -> isMeaningfulUpgrade b fac i
+          spend = \(cost,i) -> (t+1, zipWith (+) fac (unit i), zipWith (-) res (cost), sk)
+          stonks = \(x,y,z,_) -> (x,y,zipWith (+) z fac, [])
+          affordable = \(_,_,cash,_) -> all (>=0) cash
 
-hasGeode :: State -> Bool
-hasGeode (_, (_:_:_:x:_), _, _) = x > 0
+          canBuyGeode = affordable $ spend (resources !! 3)
+          buyGeode = stonks $ spend (resources !! 3)
 
-hasOre :: Int -> State -> Bool
-hasOre n (_, (x:_), _, _) = x >= n
-
-findState :: (State -> Bool) -> [Resources] -> [State] -> [State]
-findState f r [] = []
-findState f r ss@((t,_,_,_):_) = if (length states == 0) then findState f r (concat $ map (nextStates r) ss) else states
-    where states = filter f ss
+          options = map spend $ filter isUpgrade $ filter notSkipped resources
+          purchaseStates = map stonks $ filter affordable options
 
 evaluate :: Int -> [Resources] -> State -> Int
-evaluate lim r (t, _, (_:_:_:g:_),_) | t == lim = g
-evaluate lim r (t, _, (_:_:_:g:_),_) | t > lim = 0
-evaluate lim r (t, (_:_:o:_:_),_ ,_) | t > 15 && o == 0 = 0
-evaluate lim r (t, (_:_:_:g:_),_, _) | t > 22 && g == 0 = 0
+evaluate lim r (t,_,(_:_:_:g:_),_) | t == lim = g
+evaluate lim r (t,_,_,_) | t > lim = 0
 evaluate lim r s@(t,(_:_:o:g:_),_,_) = foldl max 0 $ map (evaluate lim r) $ nextStates r s
-
-evaluateWithHeuristics :: [Resources] -> Int
-evaluateWithHeuristics bp = foldl max 0 $ map (evaluate 24 bp) $ states1
-    where states1 = findState hasGeode bp $ [(0, [1,0,0,0], [0,0,0,0], [])]
 
 main = do
     content <- readFile "19.txt"
     let blueprints = unsafeParse content
-    let score = map (\bp -> fst bp * evaluate 24 (snd bp) (0, [1,0,0,0], [0,0,0,0], [])) blueprints
-    -- let score = map (\bp -> fst bp * evaluate 24 (snd bp) (0, [1,0,0,0], [0,0,0,0], [])) blueprints
-    -- takes eternity
+
+    -- (a)
     -- [0,0,6,4,30,6,14,40,63,20,33,12,0,112,30,0,187,90,152,140,21,110,92,192,50,156,324,252,145,60]
+    -- let score = map (\bp -> fst bp * evaluate 24 (snd bp) (0, [1,0,0,0], [0,0,0,0], [])) blueprints
+    -- putStrLn $ show score
+    -- putStrLn $ show $ sum score
+
+    -- (b)
+    let score = map (\bp -> evaluate 32 (snd bp) (0, [1,0,0,0], [0,0,0,0], [])) $ take 3 blueprints
     putStrLn $ show score
-    putStrLn $ show $ sum score
+    putStrLn $ show $ product score
